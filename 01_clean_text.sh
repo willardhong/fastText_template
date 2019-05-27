273c1a8ae30f0d557a948bb8604616babb9dcf11
Willard Hong <willard.hong@schwab.com>
2019-05-27 15:42:50 -0700

    My fifith commit

--- 01_clean_text.sh
+++ 01_clean_text.sh
@@ -3,8 +3,11 @@
 ### you can run in both command line by copy and paste or as ./*.sh
 ### willard hong 5/26/2019
 
-#cat data/yelp/yelp_review.csv | python parse_yelp_dataset.py > data/yelp/yelp_review.v1.csv
+###########################################
+### step 1. clean data
+###########################################
 
+#cat data/yelp/yelp_review.csv | python parse_yelp_dataset.py > data/yelp/yelp_review.v1.csv
 #cat data/yelp/yelp_review.csv | python remove_stop_words.py > data/yelp/no_stop_words.txt
 #cat data/yelp/yelp_review.v1.csv | tr '[:upper:]' '[:lower:]' > data/yelp/yelp_review.v2.csv
 
@@ -25,28 +28,52 @@
 #STR=$(wc -l < data/yelp/yelp_review.v3.csv)
 #echo $STR    
 
+### split into test and val 
+
 #awk -v fact=0.80 \
 #    'NR <= 5261668 * fact {print > "data/yelp/train.txt"; next} {print > "data/yelp/val.txt"}' \
 #    data/yelp/yelp_review.v3.csv
 
-#fasttext supervised -input data/yelp/train.txt -output result/yelp/star_model
-#fasttext test result/yelp/star_model.bin data/yelp/val.txt
+###########################################
+### step 2. build fastText model 
+###########################################
+
+#fasttext supervised -input data/yelp/train.txt -output model/yelp/star_model
+#fasttext test model/yelp/star_model.bin data/yelp/val.txt
+
+###########################################
+### step 3. build confusion matrix
+###########################################
+
 #cut -f 1 -d ' ' data/yelp/val.txt > data/yelp/val.testlabel
 #cut -f 2- -d ' ' data/yelp/val.txt > data/yelp/val.testsentences
-#fasttext predict result/yelp/star_model.bin data/yelp/val.testsentences > pexp
+#fasttext predict model/yelp/star_model.bin data/yelp/val.testsentences > pexp
 
 #python fasttext_confusion_matrix.py data/yelp/val.testlabel pexp
 
-### Hyperparameter tuning
+###########################################
+### step 4. Hyperparameter tuning
+###########################################
 
-#fasttext supervised -input data/yelp/train.txt -output result/yelp/star_model -epoch 25
+#fasttext supervised -input data/yelp/train.txt -output model/yelp/star_model -epoch 25
 
-#fasttext supervised -input data/yelp/train.txt -output result/yelp/star_model -lr 1.0
+#fasttext supervised -input data/yelp/train.txt -output model/yelp/star_model -lr 1.0
 
-#fasttext supervised -input data/yelp/train.txt -output result/yelp/star_model -wordNgrams 2
+#fasttext supervised -input data/yelp/train.txt -output model/yelp/star_model -wordNgrams 2
 
-#fasttext supervised -input data/yelp/train.txt -output result/yelp/star_model -wordNgrams 3
+#fasttext supervised -input data/yelp/train.txt -output model/yelp/star_model -wordNgrams 3
 
-#fasttext supervised -input data/yelp/train.txt -output result/yelp/star_model -wordNgrams 2 -lr 1.0 -epoch 10
+#fasttext supervised -input data/yelp/train.txt -output model/yelp/star_model -wordNgrams 2 -lr 1.0 -epoch 10
 
-#fasttext supervised -input data/yelp/train.txt -output result/yelp/star_model_withprevecs -pretrainedVectors wiki-news-300d-1M.vec -dim 300
+###########################################
+### step 5. optional ... use your own embedding
+###########################################
+#fasttext supervised -input data/yelp/train.txt -output model/yelp/star_model_withprevecs -pretrainedVectors wiki-news-300d-1M.vec -dim 300
+
+###########################################
+### step 6. Model quantization
+###########################################
+
+#fasttext quantize -output "model/yelp/star_model" -input "data/yelp/train.txt" -qnorm -retrain -epoch 1 -cutoff 100000
+
+fasttext test model/yelp/star_model.ftz data/yelp/val.txt
